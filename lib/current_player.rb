ca5a4f8099e5fd984b8142ea4241892a463139ea
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-01-13 01:21:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,13 @@
 def turn_count(board)
   count = 0
   board.each do |index|
-    if index == "X"
+    if index == "X" || index == "O"
       count += 1
-    elsif index == "O"
-      count += 1
-    end
   end
   return count
 end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
+  x = 5+7
 end
